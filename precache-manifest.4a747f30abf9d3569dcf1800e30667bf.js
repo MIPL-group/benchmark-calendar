c350d577b60298b89a7ca145ac7aca5cfbd259fa
Larry <yuetao1998@gmail.com>
2020-07-13 16:23:12 -0500

    Updates

---
+++ precache-manifest.4a747f30abf9d3569dcf1800e30667bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83cb98d186f34b56dbc9ab720514eee",
+    "url": "/benchmark-calendar/index.html"
+  },
+  {
+    "revision": "1b1d2261855121e4b9b1",
+    "url": "/benchmark-calendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d01ee917684dd3e6adf4",
+    "url": "/benchmark-calendar/static/js/2.8b1fa4c8.chunk.js"
+  },
+  {
+    "revision": "1297f9738297ced7745ecfa872c35741",
+    "url": "/benchmark-calendar/static/js/2.8b1fa4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1d2261855121e4b9b1",
+    "url": "/benchmark-calendar/static/js/main.40723d0e.chunk.js"
+  },
+  {
+    "revision": "4a56c15b7771c95905f9",
+    "url": "/benchmark-calendar/static/js/runtime-main.3ec3791f.js"
+  }
+]);
