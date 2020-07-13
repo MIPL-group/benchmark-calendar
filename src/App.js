de12a2f3432eca3ecbe56ed64cf63f6d9f1259d4
Larry <yuetao1998@gmail.com>
2020-07-13 16:43:33 -0500

    edit calendar benchmark

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Calendar from "./calendar";
+import Navbar from "react-bootstrap/Navbar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar bg="light" expand="md" fixed="top" variant="light">
+                <Navbar.Brand href="./index.html">Home</Navbar.Brand>
+            </Navbar>
+            <div className="py-5 text-center">
+                <h2 className="mt-2">Calendar</h2>
+            </div>
+            <div>
+                <Calendar/>
+            </div>
+            <footer className="my-5 pt-5 text-muted text-center text-small">
+                <p className="mb-1">Editing Calendar Benchmark Demo Site</p>
+                <a href="https://mipl-group.github.io/">MIPL github page</a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data/calendar-data.js
@@ -0,0 +1,10 @@
+export const appointments = [
+    {
+        title: 'Customer Workshop',
+        startDate: new Date(2020, 7, 13),
+        endDate: new Date(2020, 7, 13),
+        allDay: true,
+        id: 39,
+        location: 'Room 1',
+    },
+];
